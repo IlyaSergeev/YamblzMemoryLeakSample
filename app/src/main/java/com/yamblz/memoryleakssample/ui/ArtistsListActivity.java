262feb5f4315c3e77038e2ba6080d27e95d9d8a9
Ilya Sergeev <i-sergeev@yandex-team.ru>
2016-07-07 12:59:39 +0300

    remove leak from application class

--- app/src/main/java/com/yamblz/memoryleakssample/ui/ArtistsListActivity.java
+++ app/src/main/java/com/yamblz/memoryleakssample/ui/ArtistsListActivity.java
@@ -7,12 +7,12 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ProgressBar;
 
 import com.squareup.picasso.Picasso;
 import com.yamblz.memoryleakssample.R;
-import com.yamblz.memoryleakssample.SampleApplication;
 import com.yamblz.memoryleakssample.communication.Api;
 import com.yamblz.memoryleakssample.model.Artist;
 
@@ -23,6 +23,8 @@ import butterknife.ButterKnife;
 
 public class ArtistsListActivity extends AppCompatActivity
 {
+    private static final String STORED_LAST_VISIBLE_ARTIST_ID = "stored_last_visible_artist_id";
+
     @BindView(R.id.progress_bar)
     ProgressBar progressBar;
 
@@ -33,6 +35,8 @@ public class ArtistsListActivity extends AppCompatActivity
     private ArtistsAdapter artistsAdapter;
     private Api api;
     private ArtistsLoadingTask artistsLoadingTask;
+
+    private String lastVisibleArtistId = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -62,10 +66,24 @@ public class ArtistsListActivity extends AppCompatActivity
     protected void onPause()
     {
         super.onPause();
-        int firstVisiblePosition = gridLayoutManager.findFirstCompletelyVisibleItemPosition();
-        Artist firstVisibleArtist = artistsAdapter.getArtist(firstVisiblePosition);
-        ((SampleApplication)getApplication()).setFirstVisibleArtistInListActivity(firstVisibleArtist);
         resetArtistsLoadingTask();
+
+        int firstVisiblePosition = gridLayoutManager.findFirstCompletelyVisibleItemPosition();
+        lastVisibleArtistId = artistsAdapter.getArtist(firstVisiblePosition).getId();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+        lastVisibleArtistId = savedInstanceState.getString(STORED_LAST_VISIBLE_ARTIST_ID, null);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        outState.putString(STORED_LAST_VISIBLE_ARTIST_ID, lastVisibleArtistId);
     }
 
     private void resetArtistsLoadingTask()
@@ -102,12 +120,11 @@ public class ArtistsListActivity extends AppCompatActivity
         recyclerView.setAdapter(artistsAdapter);
         artistsAdapter.notifyDataSetChanged();
 
-        Artist firstVisibleArtist = ((SampleApplication)getApplication()).getFirstVisibleArtistInListActivity();
-        if (firstVisibleArtist != null)
+        if (!TextUtils.isEmpty(lastVisibleArtistId))
         {
             for (int i = 0; i < data.length; i++)
             {
-                if (data[i].getId().equals(firstVisibleArtist.getId()))
+                if (data[i].getId().equals(lastVisibleArtistId))
                 {
                     recyclerView.scrollToPosition(i);
                     break;
