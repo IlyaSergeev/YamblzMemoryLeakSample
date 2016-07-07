93d5fafe6da53dd6798255f030821c1599f8d4d1
Ilya Sergeev <i-sergeev@yandex-team.ru>
2016-07-07 11:18:31 +0300

    add gridlayoutmanager for artist

---
+++ app/src/main/java/com/yamblz/memoryleakssample/SampleApplication.java
@@ -0,0 +1,18 @@
+package com.yamblz.memoryleakssample;
+
+import android.app.Application;
+
+import com.yamblz.memoryleakssample.ui.artists_list.ArtistsListActivity;
+
+/**
+ * Created by i-sergeev on 07.07.16
+ */
+public class SampleApplication extends Application
+{
+    private static ArtistsListActivity artistsListActivity;
+
+    public static void setArtistsListActivity(ArtistsListActivity artistsListActivity)
+    {
+        SampleApplication.artistsListActivity = artistsListActivity;
+    }
+}
--- app/src/main/java/com/yamblz/memoryleakssample/ui/DividerItemDecoration.java
+++ app/src/main/java/com/yamblz/memoryleakssample/ui/DividerItemDecoration.java
@@ -4,43 +4,78 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import com.yamblz.memoryleakssample.R;
 
 /**
  * Created by i-sergeev on 01.07.16
  */
 public class DividerItemDecoration extends RecyclerView.ItemDecoration
 {
-    private Drawable drawable;
+    private Drawable mDivider;
+    private int mInsets;
 
-    /**
-     * Custom divider will be used
-     */
-    public DividerItemDecoration(Context context, int resId)
+    public DividerItemDecoration(Context context)
     {
-        drawable = ContextCompat.getDrawable(context, resId);
+        mDivider = context.getResources().getDrawable(R.drawable.divider);
+        mInsets = context.getResources().getDimensionPixelSize(R.dimen.card_insets);
     }
 
     @Override
-    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state)
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state)
     {
-        int left = parent.getPaddingLeft();
-        int right = parent.getWidth() - parent.getPaddingRight();
+        drawVertical(c, parent);
+        drawHorizontal(c, parent);
+    }
 
-        int childCount = parent.getChildCount();
+    /**
+     * Draw dividers at each expected grid interval
+     */
+    public void drawVertical(Canvas c, RecyclerView parent)
+    {
+        if (parent.getChildCount() == 0)
+        {
+            return;
+        }
+
+        final int childCount = parent.getChildCount();
+
         for (int i = 0; i < childCount; i++)
         {
-            View child = parent.getChildAt(i);
+            final View child = parent.getChildAt(i);
+            final RecyclerView.LayoutParams params =
+                    (RecyclerView.LayoutParams) child.getLayoutParams();
 
-            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+            final int left = child.getLeft() - params.leftMargin - mInsets;
+            final int right = child.getRight() + params.rightMargin + mInsets;
+            final int top = child.getBottom() + params.bottomMargin + mInsets;
+            final int bottom = top + mDivider.getIntrinsicHeight();
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
 
-            int top = child.getBottom() + params.bottomMargin;
-            int bottom = top + drawable.getIntrinsicHeight();
+    /**
+     * Draw dividers to the right of each child view
+     */
+    public void drawHorizontal(Canvas c, RecyclerView parent)
+    {
+        final int childCount = parent.getChildCount();
 
-            drawable.setBounds(left, top, right, bottom);
-            drawable.draw(c);
+        for (int i = 0; i < childCount; i++)
+        {
+            final View child = parent.getChildAt(i);
+            final RecyclerView.LayoutParams params =
+                    (RecyclerView.LayoutParams) child.getLayoutParams();
+
+            final int left = child.getRight() + params.rightMargin + mInsets;
+            final int right = left + mDivider.getIntrinsicWidth();
+            final int top = child.getTop() - params.topMargin - mInsets;
+            final int bottom = child.getBottom() + params.bottomMargin + mInsets;
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
         }
     }
 
@@ -50,13 +85,7 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration
                                RecyclerView parent,
                                RecyclerView.State state)
     {
-        super.getItemOffsets(outRect, view, parent, state);
-
-        if (parent.getChildAdapterPosition(view) == 0)
-        {
-            return;
-        }
-
-        outRect.top = drawable.getIntrinsicHeight();
+        //We can supply forced insets for each item view here in the Rect
+        outRect.set(mInsets, mInsets, mInsets, mInsets);
     }
 }
