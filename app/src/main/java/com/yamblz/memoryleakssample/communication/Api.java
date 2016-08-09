44ae154ca887b10febbc6867295a093538edd8ee
Veda Voronina <veda345@yandex.ru>
2016-08-09 13:33:55 +0300

    streams

--- app/src/main/java/com/yamblz/memoryleakssample/communication/Api.java
+++ app/src/main/java/com/yamblz/memoryleakssample/communication/Api.java
@@ -7,6 +7,7 @@ import com.google.gson.Gson;
 import com.yamblz.memoryleakssample.R;
 import com.yamblz.memoryleakssample.model.Artist;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
@@ -27,18 +28,14 @@ public class Api
 
     public Artist[] getArtists() {
 
-//        try
-//        {
-//            Thread.sleep(3000);
-//        }
-//        catch (InterruptedException e)
-//        {
-//            e.printStackTrace();
-//        }
-
         InputStream inStream = context.getResources().openRawResource(R.raw.artists);
         InputStreamReader inStreamReader = new InputStreamReader(inStream);
-
-        return gson.fromJson(inStreamReader, Artist[].class);
+        Artist[] artists = gson.fromJson(inStreamReader, Artist[].class);
+        try {
+            inStreamReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return artists;
     }
 }
